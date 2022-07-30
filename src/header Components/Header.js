fe6d1325251a96d45d0aea3d6a0454b41c682840
nthalamanchi <nthalamanchi@gmail.com>
2022-07-30 14:49:04 +0530

    testing3

--- src/header Components/Header.js
+++ src/header Components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Header hai extends Component {
+export default class Header extends Component {
   render() {
     return (
       <div>Header hai</div>
